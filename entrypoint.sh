b15c27d7f8b654c730ac0e8c5fe204b869dde54d
SYKuang <sykuang@gmail.com>
2018-02-24 01:08:17 +0800

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ ! -f /etc/ssh/ssh_host_rsa_key] ; then
+if [ ! -f /etc/ssh/ssh_host_rsa_key ] ; then
 # Generate ssh key
-    ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key
-    ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key
-    ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key
+    ssh-keygen -N '' -t rsa -f /etc/ssh/ssh_host_rsa_key
+    ssh-keygen -N '' -t dsa -f /etc/ssh/ssh_host_dsa_key
+    ssh-keygen -N '' -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key
 fi
 /usr/bin/supervisord
