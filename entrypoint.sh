0bacb7249bb70faba4ede646c6e732d67e8017b1
sykuang <sykuang.tw@gmail.com>
2022-09-06 23:57:08 +0800

    Add password

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,8 @@ if [ ! -f /etc/ssh/ssh_host_rsa_key ] ; then
     ssh-keygen -N '' -t dsa -f /etc/ssh/ssh_host_dsa_key
     ssh-keygen -N '' -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key
 fi
+if [[ -e $PASSWORD ]];then
+  x11vnc -storepasswd  "$PASSWORD"
+  echo "docker:1234" | chpasswd
+fi
 /usr/bin/supervisord
